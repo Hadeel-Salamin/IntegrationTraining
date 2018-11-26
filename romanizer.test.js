6410335bfa332f8f364e4779e14429138debec23
hadeel_salamin <hadeel_salamin@hotmail.com>
2018-11-26 14:21:48 +0200

    solving errors

--- romanizer.test.js
+++ romanizer.test.js
@@ -32,48 +32,48 @@ test('Testing romanizer function output III', function (t) {
 test('Testing romanizer function output IV',function(t) {
   var actual=romanizer(4);
   var expected='IV';
-  t.equal(actual,expected,'4 should equal IV')
+  t.equal(actual,expected,'4 should equal IV');
   t.end();
 });
 
 test('Testing romanizer function output IV',function(t) {
   var actual=romanizer(5);
   var expected='V';
-  t.equal(actual,expected,'5 should equal V')
+  t.equal(actual,expected,'5 should equal V');
   t.end();
 });
 
 test('Testing romanizer function output VI',function(t) {
   var actual=romanizer(6);
   var expected='VI';
-  t.equal(actual,expected,'6 should equal VI')
+  t.equal(actual,expected,'6 should equal VI');
   t.end();
-})
+});
 
 test('Testing romanizer function output VII',function(t) {
   var actual=romanizer(7);
   var expected='VII';
-  t.equal(actual,expected,'7 should equal VII')
+  t.equal(actual,expected,'7 should equal VII');
   t.end();
-})
+});
 
 test('Testing romanizer function output VIII',function(t) {
   var actual=romanizer(8);
   var expected='VIII';
-  t.equal(actual,expected,'8 should equal VIII')
+  t.equal(actual,expected,'8 should equal VIII');
   t.end();
-})
+});
 
 test('Testing romanizer function output IX',function(t) {
   var actual=romanizer(9);
   var expected='IX';
-  t.equal(actual,expected,'9 should equal IX')
+  t.equal(actual,expected,'9 should equal IX');
   t.end();
-})
+});
 
 test('Testing romanizer function output X',function(t) {
   var actual=romanizer(10);
   var expected='X';
-  t.equal(actual,expected,'10 should equal X')
+  t.equal(actual,expected,'10 should equal X');
   t.end();
-})
+});
