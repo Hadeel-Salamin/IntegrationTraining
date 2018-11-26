e5a8e9fbd3979117768b4a5a9d03e59478a8e1b0
hadeel_salamin <hadeel_salamin@hotmail.com>
2018-11-26 14:19:07 +0200

    adding travis test

--- romanizer.test.js
+++ romanizer.test.js
@@ -6,42 +6,42 @@ test('Testing Tape is working', function (t) {
   var expected=2;
   t.equal(actual, expected, '2 should equal 2 ');
   t.end();
-})
+});
 
 test('Testing romanizer function output I', function (t) {
   var actual=romanizer(1);
   var expected='I';
   t.equal(actual, expected, '1 should equal I');
   t.end();
-})
+});
 
 test('Testing romanizer function output II', function (t) {
   var actual=romanizer(2);
   var expected='II';
   t.equal(actual, expected, '2 should equal II');
   t.end();
-})
+});
 
 test('Testing romanizer function output III', function (t) {
   var actual=romanizer(3);
   var expected='III';
   t.equal(actual, expected, '3 should equal III');
   t.end();
-})
+});
 
 test('Testing romanizer function output IV',function(t) {
   var actual=romanizer(4);
   var expected='IV';
   t.equal(actual,expected,'4 should equal IV')
   t.end();
-})
+});
 
 test('Testing romanizer function output IV',function(t) {
   var actual=romanizer(5);
   var expected='V';
   t.equal(actual,expected,'5 should equal V')
   t.end();
-})
+});
 
 test('Testing romanizer function output VI',function(t) {
   var actual=romanizer(6);
